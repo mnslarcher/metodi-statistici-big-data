5ed1bf225048849eeef9e197dadfa33c0df79e8f
mnslarcher <24874992+mnslarcher@users.noreply.github.com>
2019-03-17 17:19:24 +0100

    piccole var

--- msbd/grafici/stimatori.py
+++ msbd/grafici/stimatori.py
@@ -2,18 +2,18 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 
-def grafico_coefficienti(coef, nomi):
+def grafico_coefficienti(coefficienti, variabili):
     """Grafico dei coefficienti
 
     Parameters
     ----------
-    coef : array-like
+    coefficienti : array-like
         Valori dei coefficienti.
-    nomi : array-like
+    variabili : array-like
         Nomi delle variabili legate ai coefficienti.
 
     """
-    coef = pd.Series(coef, nomi).sort_values()
+    coef = pd.Series(coefficienti, variabili).sort_values()
     coef.plot(kind='bar', title='Grafico dei coefficienti',
         color=[["tab:blue", "tab:orange"][i] for i in (coef > 0)])
     plt.grid()
