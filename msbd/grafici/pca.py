a0aeceddf8a64847d37ec127e6ad01495939ff64
mnslarcher <24874992+mnslarcher@users.noreply.github.com>
2019-04-07 17:24:33 +0200

    grafico varianza spiegata

--- msbd/grafici/pca.py
+++ msbd/grafici/pca.py
@@ -2,6 +2,23 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
+def grafico_varianza_spiegata(varianza_spiegata, cumulata=False):
+    titolo = "% varianza spiegata"
+
+    if cumulata:
+        titolo += " cumulata al variare del numero di componenti"
+        varianza_spiegata = varianza_spiegata.cumsum()
+    else:
+        titolo += " da ogni componente"
+
+    plt.title(titolo)
+    plt.plot(range(1, len(varianza_spiegata) + 1), varianza_spiegata,
+        marker='o', ls='--')
+    plt.xlabel("Componenti")
+    plt.ylabel("% varianza spiegata")
+    plt.grid()
+
+
 def grafico_proiezione_sul_primo_asse_principale(X):
     U, s, Vt = np.linalg.svd(X, full_matrices=False)
     V = Vt.T
