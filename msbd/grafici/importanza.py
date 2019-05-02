876df50581e29464ba58b329e094b1741e6c0eb1
mnslarcher <24874992+mnslarcher@users.noreply.github.com>
2019-05-02 19:25:19 +0200

    spostato il titolo tra i params

--- msbd/grafici/importanza.py
+++ msbd/grafici/importanza.py
@@ -1,8 +1,9 @@
 import pandas as pd
 
 
-def grafico_importanza_variabili(importanze, variabili, max_num=None):
-        importanze = pd.Series(importanze, index=variabili).sort_values(
-            ascending=False)
-        importanze[:max_num].plot(kind="bar", grid=True,
-            title="Importanza delle variabili", color="tab:blue")
+def grafico_importanza_variabili(importanze, variabili, max_num=None,
+        titolo="Importanza delle variabili"):
+    importanze = pd.Series(importanze, index=variabili).sort_values(
+        ascending=False)
+    importanze[:max_num].plot(kind="bar", grid=True, title=titolo,
+        color="tab:blue")
