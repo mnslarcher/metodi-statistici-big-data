991b070132572f915a9ddf5c962de29ee92ca123
mnslarcher <24874992+mnslarcher@users.noreply.github.com>
2019-05-02 19:24:03 +0200

    roc precision recall

--- msbd/grafici/metriche.py
+++ msbd/grafici/metriche.py
@@ -1,13 +1,48 @@
 from itertools import product
+from sklearn.metrics import auc
+from sklearn.metrics import average_precision_score
 from sklearn.metrics import confusion_matrix
+from sklearn.metrics import precision_recall_curve
+from sklearn.metrics import roc_curve
 import matplotlib.pyplot as plt
 
 
+def grafico_curva_precisione_richiamo(y_true, y_score,
+        titolo="Curva precisione-richiamo"):
+    plt.title(titolo)
+    precisione, richiamo, _ = precision_recall_curve(y_true, y_score)
+    plt.plot(richiamo, precisione, lw=2, label="Classificatore (AP = {:.2f})".\
+        format(average_precision_score(y_true, y_score)))
+    baseline = y_true.mean()
+    plt.plot([0, 1], [baseline, baseline], lw=2, ls="--",
+        label="Baseline (AP = {:.2f})".format(baseline))
+    plt.xlabel("Richiamo")
+    plt.ylabel("Precisione")
+    plt.xlim([-0.05, 1.05])
+    plt.ylim([-0.05, 1.05])
+    plt.grid()
+    plt.legend()
+
+
+def grafico_curva_roc(y_true, y_score, titolo="Curva ROC"):
+    plt.title(titolo)
+    tfp, tvp, _ = roc_curve(y_true, y_score)
+    plt.plot(tfp, tvp, lw=2, label="Classificatore (AUC = {:.2f})".format(
+        auc(tfp, tvp)))
+    plt.plot([0, 1], [0, 1], lw=2, ls="--", label="Baseline (AUC = 0.5)")
+    plt.xlabel("Tasso falsi positivi")
+    plt.ylabel("Tasso veri positivi (richiamo)")
+    plt.xlim([-0.05, 1.05])
+    plt.ylim([-0.05, 1.05])
+    plt.grid()
+    plt.legend()
+
+
 def grafico_matrice_confusione(y_true, y_pred, classi,
-    titolo="Matrice di confusione", cmap=plt.cm.Blues):
+        titolo="Matrice di confusione", cmap=plt.cm.Blues):
+    plt.title(titolo)
     cm = confusion_matrix(y_true, y_pred)
     plt.imshow(cm, interpolation='nearest', cmap=cmap)
-    plt.title(titolo)
     plt.colorbar(fraction=0.04, pad=0.2)
     tick_marks = range(len(classi))
     plt.xticks(tick_marks, classi, rotation=90)
